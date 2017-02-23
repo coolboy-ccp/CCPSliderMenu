fea0e68b1207746100aa28a8007caf1df4689ee4
储诚鹏 <chuchengpeng0119@163.com>
2017-02-23 13:25:38 +0800

    Initial Commit

---
+++ CCPSliderMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CCPSliderMenu
+//
+//  Created by 储诚鹏 on 17/2/23.
+//  Copyright © 2017年 chuchengpeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
